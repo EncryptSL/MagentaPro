8b322af5fd2b1081090ac99c35233354da8e0bd0
EncryptSL <Rydlos123@gmail.com>
2023-09-24 23:50:43 +0200

    Bump another..

--- src/main/kotlin/com/github/encryptsl/magenta/api/KitManager.kt
+++ src/main/kotlin/com/github/encryptsl/magenta/api/KitManager.kt
@@ -2,26 +2,55 @@ package com.github.encryptsl.magenta.api
 
 import com.github.encryptsl.magenta.Magenta
 import com.github.encryptsl.magenta.common.utils.ModernText
+import net.kyori.adventure.text.Component
 import org.bukkit.Material
-import org.bukkit.NamespacedKey
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 
 class KitManager(private val magenta: Magenta) {
 
-    private val itemFactory = ItemFactory()
-
     fun giveKit(player: Player, kitName: String) {
+        val inv: Inventory = player.inventory
 
-        runCatching {
-            magenta.kitConfig.getKit().getConfigurationSection("kits")
-        }.onSuccess { section ->
-            val k = section?.get("kits.$kitName") as HashMap<String, Any>
-            val items = k["items"] as List<HashMap<String, Any>>
-            items.forEach { item ->
-                println(item["id"])
+        if (!magenta.kitConfig.getKit().contains("kits.$kitName"))
+            return player.sendMessage(ModernText.miniModernText(magenta.localeConfig.getMessage("magenta.command.kit.error.not.exist")))
+
+        for (material in Material.entries) {
+            val amount: Int = magenta.kitConfig.getKit().getInt(("kits.$kitName.items.${material.name}.amount"))
+            val item = ItemStack(material, amount)
+            val meta: ItemMeta = item.itemMeta
+            if (magenta.kitConfig.getKit().contains("kits.$kitName.items.${material.name}.displayName")) {
+                meta.displayName(
+                    ModernText.miniModernText(
+                        magenta.kitConfig.getKit().getString("kits.$kitName.items.${material.name}.meta.displayName") ?: material.name
+                    )
+                )
             }
-        }.exceptionOrNull()
+            if (magenta.kitConfig.getKit().contains(("kits.$kitName.items.${material.name}.meta.lore"))) {
+                val loreItems: List<String> = magenta.kitConfig.getKit()
+                    .getStringList("kits.tools.items.stone_pickaxe.meta.lore")
+                val newList: MutableList<Component> = ArrayList()
+                for (loreItem in loreItems) {
+                    newList.add(ModernText.miniModernText(loreItem))
+                }
+                meta.lore(newList)
+            }
+            item.setItemMeta(meta)
+            for (enchantment in Enchantment.values()) {
+                if (magenta.kitConfig.getKit()
+                    .contains("kits.$kitName.items.${material.name}.enchants.${enchantment.getKey().getKey()}")) {
+                    item.addEnchantment(
+                        enchantment,
+                        magenta.kitConfig.getKit()
+                            .getInt("kits.$kitName.items.${material.name}.enchants.${enchantment.getKey().getKey()}")
+                    )
+                }
+            }
+            inv.addItem(item)
+        }
     }
 
     fun createKit(kitName: String) {
