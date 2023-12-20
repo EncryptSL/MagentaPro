6bf74464aa9988ae5af74e62b3ec8ac3560a049b
EncryptSL <Rydlos123@gmail.com>
2023-12-20 15:16:38 +0100

    fix

--- src/main/kotlin/com/github/encryptsl/magenta/api/events/clan/ClanChangeRoleEvent.kt
+++ src/main/kotlin/com/github/encryptsl/magenta/api/events/clan/ClanChangeRoleEvent.kt
@@ -1,12 +1,11 @@
 package com.github.encryptsl.magenta.api.events.clan
 
-import com.github.encryptsl.magenta.api.clan.ClanRoles
 import org.bukkit.entity.Player
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class ClanChangeRoleEvent(val player: Player, val clanName: String, val roles: ClanRoles) : Event(), Cancellable {
+class ClanChangeRoleEvent(val player: Player, val clanName: String) : Event(), Cancellable {
     private var isCancelled: Boolean = false
     override fun getHandlers(): HandlerList = handlerList
     override fun isCancelled(): Boolean = isCancelled
