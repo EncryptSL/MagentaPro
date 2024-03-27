ef7a959a3e3048a56af09d9164083801f83e1cd2
EncryptSL <Rydlos123@gmail.com>
2024-03-27 12:40:15 +0100

    Vote Milestones gui removed from config and moved into separate config
    Refactoring of code in VoteMilestonesGUI and other stuff...

--- src/main/kotlin/com/github/encryptsl/magenta/cmds/VoteCmd.kt
+++ src/main/kotlin/com/github/encryptsl/magenta/cmds/VoteCmd.kt
@@ -2,7 +2,7 @@ package com.github.encryptsl.magenta.cmds
 
 import com.github.encryptsl.magenta.Magenta
 import com.github.encryptsl.magenta.api.commands.AnnotationFeatures
-import com.github.encryptsl.magenta.api.menu.vote.VoteMilestonesGUI
+import com.github.encryptsl.magenta.api.menu.milestones.VoteMilestonesGUI
 import com.github.encryptsl.magenta.common.hook.nuvotifier.VoteHelper
 import com.github.encryptsl.magenta.common.utils.ModernText
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
@@ -51,7 +51,7 @@ class VoteCmd(val magenta: Magenta) : AnnotationFeatures {
     @Permission("magenta.vote.milestones")
     @CommandDescription("This command open vote milestones")
     fun onVoteMilestones(player: Player) {
-        voteMilestonesGUI.openVoteMilestonesGui(player)
+        voteMilestonesGUI.openMenu(player)
     }
 
     @Command("vote claim rewards")
