90795ef75c9cb26596c29b821fb13ec2bed51cd4
EncryptSL <Rydlos123@gmail.com>
2024-05-10 02:00:35 +0200

    Fixed yaml loading from file paper-libraries.yml

--- src/main/java/com/github/encryptsl/magenta/MagentaLoader.java
+++ src/main/java/com/github/encryptsl/magenta/MagentaLoader.java
@@ -7,13 +7,13 @@ import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
 @SuppressWarnings({"UnstableApiUsage", "unused"})
 public class MagentaLoader implements PluginLoader {
@@ -24,9 +24,6 @@ public class MagentaLoader implements PluginLoader {
         resolveLibraries().stream()
                 .map(DefaultArtifact::new)
                 .forEach(artifact -> resolver.addDependency(new Dependency(artifact, null)));
-        resolver.addRepository(new RemoteRepository.Builder(
-                "maven", "default", "https://repo.maven.apache.org/maven2/"
-        ).build());
 
         resolver.addRepository(new RemoteRepository.Builder("paper", "default", "https://repo.papermc.io/repository/maven-public/").build());
         pluginClasspath.addLibrary(resolver);
@@ -42,20 +39,17 @@ public class MagentaLoader implements PluginLoader {
     }
 
     private List<String> readLibraryListFromYaml() throws IOException {
-        List<String> libraries = new ArrayList<>();
+        Yaml yaml = new Yaml();
         InputStream inputStream = MagentaLoader.class.getClassLoader()
                 .getResourceAsStream("paper-libraries.yml");
 
-        if (inputStream != null) {
-            try (inputStream; Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8)) {
-                while (scanner.hasNextLine()) {
-                    libraries.add(scanner.nextLine());
-                }
-            }
-        } else {
+        if (inputStream == null) {
             System.err.println("paper-libraries.yml not found in the classpath.");
         }
-        return libraries;
+
+        Map<String, List<String>> data = yaml.load(inputStream);
+
+        return data.get("libraries");
     }
 
 }
