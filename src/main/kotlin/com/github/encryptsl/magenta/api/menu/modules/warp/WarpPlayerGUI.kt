7138154c00b36bec016ffa3e94d854cac2d63c46
EncryptSL <Rydlos123@gmail.com>
2024-07-22 04:14:36 +0200

    Added RegistryAccess.

--- src/main/kotlin/com/github/encryptsl/magenta/api/menu/modules/warp/WarpPlayerGUI.kt
+++ src/main/kotlin/com/github/encryptsl/magenta/api/menu/modules/warp/WarpPlayerGUI.kt
@@ -22,6 +22,7 @@ class WarpPlayerGUI(private val magenta: Magenta, private val warpGUI: WarpGUI, 
     override fun open(player: Player) {
         val rows = magenta.homeMenuConfig.getConfig().getInt("menu.gui.size", 6)
         val playerWarps = magenta.warpModel.getWarpsByOwner(player.uniqueId).join()
+        val warpsIterator = playerWarps.iterator()
 
         val gui = menuUI.paginatedBuilderGui(rows,
             ModernText.miniModernText(magenta.warpPlayerMenuConfig.getConfig().getString("menu.gui.display").toString(),
@@ -34,7 +35,8 @@ class WarpPlayerGUI(private val magenta: Magenta, private val warpGUI: WarpGUI, 
         if (!magenta.warpPlayerMenuConfig.getConfig().contains("menu.warp-info.display")) return
         if (!magenta.warpPlayerMenuConfig.getConfig().contains("menu.warp-info.lore")) return
 
-        for (warp in playerWarps) {
+        while (warpsIterator.hasNext()) {
+            val warp = warpsIterator.next()
             val material = Material.getMaterial(warp.warpIcon) ?: Material.OAK_SIGN
 
             val displayItemName = ModernText.miniModernText(magenta.warpPlayerMenuConfig.getConfig().getString("menu.warp-info.display").toString(),
@@ -52,7 +54,7 @@ class WarpPlayerGUI(private val magenta: Magenta, private val warpGUI: WarpGUI, 
                     Placeholder.parsed("z", warp.z.toString()),
                     Placeholder.parsed("yaw", warp.yaw.toString()),
                     Placeholder.parsed("pitch", warp.pitch.toString()),
-                    )) }
+                )) }
 
             val itemBuilder = ItemCreator(material).setName(displayItemName)
                 .addLore(lore.toMutableList())
